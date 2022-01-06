1d8c474782dcfc42f738805d83f9f0c51d496447
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2022-01-06 17:10:47 +0100

    Added moralis dependencies and added alt

--- components/Login.tsx
+++ components/Login.tsx
@@ -7,12 +7,12 @@ function Login() {
         <div className="bg-black relative">
             <h1>I am login</h1>
             <div className="flex flex-col absolute z-50 items-center justify-center w-full h-4/6 space-y-4">
-                <Image className="object-cover rounded-full" src="https://links.papareact.com/3pi" width={200} height={200} objectFit="cover"/>
+                <Image alt="Papafam" className="object-cover rounded-full" src="https://links.papareact.com/3pi" width={200} height={200} objectFit="cover"/>
                 <button onClick={() => authenticate()} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login to the METAVERSE</button>
             </div>
 
             <div className="w-full h-screen">
-                <Image src="https://links.papareact.com/55n" layout="fill" objectFit="cover"/>
+                <Image alt="Background" src="https://links.papareact.com/55n" layout="fill" objectFit="cover"/>
             </div>
         </div>
     )
