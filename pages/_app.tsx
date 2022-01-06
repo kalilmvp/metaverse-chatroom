92912de98e0070eeb090882269807baa15927b5c
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2022-01-06 16:58:28 +0100

    Initial commit for the chatroom on web 3.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const appId: string = String(process.env.NEXT_PUBLIC_APP_ID);
+  const serverUrl: string = String(process.env.NEXT_PUBLIC_SERVER_URL);
+  
+  return (
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
+  
 }
 
 export default MyApp
